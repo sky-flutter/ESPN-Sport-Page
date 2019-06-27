2e61a063dcf996966b87fc70ec3481b2283dea68
Aakash Kareliya <aakash@parcelpal.com>
2019-06-27 10:35:06 +0530

    ESPN Sport App UI

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'HomePage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
---
+++ lib/utils/CustomColors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static var COLOR_FB = Color(0xFF3b5998);
+  static var COLOR_GREEN = Color(0xFF01a550);
+}
